e4eac7de25489ba017f865f14e1517abbd7b5117
MCroFor <marcia.for76@gmail.com>
2025-09-29 22:01:11 -0300

    Adicionando testes de transferência

--- test/login.test.js
+++ test/login.test.js
@@ -6,7 +6,7 @@ describe('Login', () => {
         it('Deve retornar 200 com um token em string quando usar credenciais válidas', async () => {
             const resposta = await request('http://localhost:3000')
                 .post('/login')
-                .set('Content-Type', 'application/json')
+                .set('Content-Type', 'application/json')                
                 .send({
                     'username':'julio.lima',
                     'senha': '123456'
